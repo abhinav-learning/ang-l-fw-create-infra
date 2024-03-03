9fd3aa9c67e9e8aaefeaf9fbed3cd64f410cb41a
Abhinav Nath Gupta <abhi.aec89@gmail.com>
2024-03-03 19:27:22 +0530

    ECR Creation Pulumi Script

--- Pulumi/create-ecr/index.ts
+++ Pulumi/create-ecr/index.ts
@@ -1,28 +1,13 @@
 import * as aws from "@pulumi/aws";
 
-// Create an ECR repository
-const repository = new aws.ecr.Repository("my-repository", {
-    imageScanningConfiguration: { scanOnPush: true },
+// Create an ECR Repository named 'ang-l-fw'
+const repository = new aws.ecr.Repository("ang", {
+    name: "ang-l-fw",
+    imageScanningConfiguration: {
+        scanOnPush: true,
+    },
     imageTagMutability: "MUTABLE",
-    // ECR repositories are immutable by default, but can be set to mutable.
 });
 
-// Get an authentication token for the ECR repository
-const authToken = aws.ecr.getCredentials({
-    registryId: repository.registryId,
-}, { async: true }).then(creds => creds.authorizationToken);
-
-// Store the non-expiring authentication token in AWS Secrets Manager
-const secret = new aws.secretsmanager.Secret("ecrAuthToken", {
-    name: "ecrAuthToken",
-    description: "ECR repository authentication token",
-});
-
-new aws.secretsmanager.SecretVersion("ecrAuthTokenVersion", {
-    secretId: secret.id,
-    secretString: authToken,
-});
-
-// Export the repository URL and secret ARN
+// Export the repository URL
 export const repositoryUrl = repository.repositoryUrl;
-export const secretArn = secret.arn;
